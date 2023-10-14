e926ede1ba3ad383ed4acdbeb95ec81568018ac2
probablyjassin <aouanijassin@gmail.com>
2023-10-14 16:31:06 +0200

    fixed secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         'text': '#131217',
         'background': '#e5e0f1',
         'primary': '#e80001',
-        'secondary': '#033d5a',
+        'secondary': '#f3a5a5',
         'accent': '#200082',
        },       
     },
