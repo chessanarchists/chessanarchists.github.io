b3224b30e3c8106865115f8e5e6ba99a2fab28cf
Bishop-up-my-ass <147948344+Bishop-up-my-ass@users.noreply.github.com>
2023-10-20 09:43:54 -0400

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,23 @@
+
+<template>
+    <div class="about-card">
+        <div class="card-title">
+            <img src=""
+        <p class="text-3xl">About page</p>
+        <nuxt-link to="/"  class="text-blue-500">go back</nuxt-link>
+        <main>
+            <ContentDoc/>
+        </main>
+    </div>
+</template>
+
+#^jassin did
+#trying to make cool about
+
 import Vue from 'vue'
 import router from 'vue-router'
 import about from './views/about.vue'
+
 
 vue.use(router)
 
