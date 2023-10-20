fb2c18168a8c170c81200b4817466c7f14d481d2
Bishop-up-my-ass <147948344+Bishop-up-my-ass@users.noreply.github.com>
2023-10-19 23:15:39 -0400

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -4,3 +4,304 @@
         <nuxt-link to="/"  class="text-blue-500">go back</nuxt-link>
     </div>
 </template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Peluso Construction is comprised of a small team with over 20 years experience in commercial renovations in healthcare and other specialized settings." />
+	<meta name="keywords" content="team, experts, experience, commercial renovations, healthcare, specialized, Michael Peluso, Dennis Atkinson, Shawn Carignan, Keith Long, James Peluso, Jeffrey Tully, experts, professional, architects, mechanical, plumbing, electrical, drywall, flooring, painting, subcontractors, Massachusetts" />
+	
+	<title>Peluso Construction, Inc.</title>
+	
+	<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+<div id="container">
+    <header>
+			<img src="images/peluso_construction_logo.gif" alt="Peluso Construction, Inc."/>
+		<nav>
+        	<ul>
+				<li><a href= "index.html">Home</a></li>
+        		<li><a href= "services_and_clients.html">Services and Clients</a></li>
+        		<li><a href= "about_us.html">About Us</a></li>
+        		<li><a href= "contact_us.html">Contact Us</a></li>
+        	</ul>
+    	</nav>
+    </header>
+    
+	<div id="mainContent">
+	
+		<!-- Main Content -->
+		
+		<h1>Construction Management Professionals</h1>	
+		<h2>About Us</h2>
+			<p>Peluso Construction, Inc. is a general contractor with over 20 years experience in commercial renovations in healthcare and other specialized settings since 1985.
+			Peluso Construction, Inc. offers competitive pricing through competitive bidding. We provide project management of all phases of the construction project so the client can focus on service delivery.</p>
+		<h3>Our Team</h3>
+				<p>Peluso Construction is comprised of a small team with over 20 years experience in commercial renovations in healthcare and other specialized settings.</p>
+								<ul>
+									<li><strong>Michael Peluso, </strong><em>President</em> 
+									<br/>Has been contracting commercial construction projects since 1985</li>
+								
+									<li><strong>Dennis Atkinson, </strong><em>Project Manager</em> 
+									<br/>Has been with Peluso Construction since 1985</li>
+								
+									<li><strong>Shawn Carignan, </strong><em>Project Manager</em> 
+									<br/>Has over 20 years experience in health care settings- joined Peluso Construction in 2008</li>
+								
+									<li><strong>Keith Long, </strong><em>Project Supervisor</em> 
+									<br/>Has been with Peluso Construction since 	1995</li>
+								
+									<li><strong>James Peluso, </strong><em>Project Supervisor</em> 
+									<br/>Has been with Peluso Construction since 2006</li>
+								
+									<li><strong>Jeffrey Tully, </strong><em>Project Supervisor</em> 
+									<br/>Has been with Peluso Construction since 2007</li>
+								</ul>
+			<h3>Our Associates</h3>
+							<p>In addition to our team of experts, we have long standing relationships with professional architects and engineers for design build projects, as well as the finest mechanical, plumbing, electrical, drywall, flooring and painting subcontractors located in eastern and central Massachusetts.</p>
+
+	</div> <!-- END #mainContent -->
+	<footer>
+		Peluso Construction, Inc. is fully licensed (CS00011737) and insured.
+	</footer>
+</div> <!-- End #container -->
+</body>
+</html>
+contact_us.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Contact Peluso Construction, Inc." />
+	<meta name="keywords" content="Address, Oakham, MA, Phone, Fax, Name, Email, Questions, Comments" />
+	
+	<title>Peluso Construction, Inc.</title>
+	
+	<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+<div id="container">
+    <header>
+			<img src="images/peluso_construction_logo.gif" alt="Peluso Construction, Inc."/>
+		<nav>
+        	<ul>
+				<li><a href= "index.html">Home</a></li>
+        		<li><a href= "services_and_clients.html">Services and Clients</a></li>
+        		<li><a href= "about_us.html">About Us</a></li>
+        		<li><a href= "contact_us.html">Contact Us</a></li>
+        	</ul>
+    	</nav>
+    </header>
+    
+	<div id="mainContent">
+	
+		<!-- Main Content -->
+		<h1>Construction Management Professionals</h1>			
+		<h2>Contact Us</h2>
+			<p>Peluso Construction works with clients across central and eastern Massachusetts. Call/fax us or fill out the form below for more information about our services, experience, and bids.</p>
+			<h3>Address</h3>
+				<p> Peluso Construction, Inc.
+				<br/>81 Bechan Rd.
+				<br/>Oakham, MA 01068</p>
+				<p><a href= "http://bit.ly/pelusoconstructioninc">Google Maps</a></p>
+			<h3>Phone and Fax</h3>
+				<p>508-882-3174
+				<br/>508-882-3914</p>
+			<h3>Contact</h3>
+				<form action="#" method="get" id="myForm">
+					<p><label>Company Name:</label>
+					<input type="text" name="company_name"/></p>
+					<p><label>First Name:</label>
+          			<input type="text" name="first_name"/></p>
+          			<p><label>Last Name:</label>
+					<input type="text" name="last_name"/></p>
+					<p><label>Phone:</label>
+					<input type="text" name="phone"/></p>
+					<p><label>Email:</label>
+					<input type="text" name="email"/></p>
+					<p><label>Questions Or Comments:</label>
+					<textarea></textarea></p>
+					
+					<p><em>Would you prefer to be contacted by phone or email?</em></p>
+					<p><label>Contact me by phone:</label>
+					<input type="checkbox" name="contact" value="Phone" /></p>
+					<p><label>Contact me by email:</label>
+					<input type="checkbox" name="contact" value="Email" /></p>
+					<p><label>No preference:</label>
+					<input type="checkbox" name="contact" value="Either" /></p>
+					
+        			<p><input type="submit" value="Submit"/></p>
+        			</form>
+				<p> How did you hear about us?</p>
+					<select>
+  						<option>Angie's List</option>
+  						<option>Yelp</option>
+  						<option>From A Former Client</option>
+  						<option>Other</option>
+  					</select>
+  				<p></p>
+    
+	</div> <!-- END #mainContent -->
+	<footer>
+		Peluso Construction, Inc. is fully licensed (CS00011737) and insured.
+	</footer>
+</div> <!-- End #container -->
+</body>
+</html>
+index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Home page for Peluso Construction, Inc. Construction Management professionals specializing in healthcare and biotechnical projects." />
+	<meta name="keywords" content="commercial, contracting, healthcare, Biotechnical, Biotech, Michael Peluso, linear accelerators, clean rooms, MRI’s, data processing facilities, professionals, contractors, specialists" />
+	
+	<title>Peluso Construction, Inc.</title>
+	
+	<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+<div id="container">
+    <header>
+			<img src="images/peluso_construction_logo.gif" alt="Peluso Construction, Inc."/>
+		<nav>
+        	<ul>
+        		<li><a href= "index.html">Home</a></li>
+        		<li><a href= "services_and_clients.html">Services and Clients</a></li>
+        		<li><a href= "about_us.html">About Us</a></li>
+        		<li><a href= "contact_us.html">Contact Us</a></li>
+        	</ul>
+    	</nav>
+    </header>
+    
+	<div id="mainContent">
+	
+		<!-- Main Content -->
+		<h1>Construction Management Professionals</h1>
+		<h2>Who We Are</h2>
+							<p>Peluso Construction is a mid-sized Commercial Contracting Company specializing in Healthcare and Biotech Projects. Founded and managed by Michael Peluso since its inception in 1985, Peluso Construction has successfully completed projects ranging from linear accelerators to biotech. manufacturing clean rooms, and MRI’s to data processing facilities.  We are proud to count among our clients some of the premier service providers in the region.</p>
+							
+							<p>Our long-standing relationships with top design professionals and quality sub-contractors allow us to excel, whether it’s on a competitive bid project or design-build.  Peluso Construction manages all phases of the construction project, allowing our clients to keep their focus on their business.</p>
+		
+		<h3><a href= "services_and_clients.html">Services and Clients</a></h3>
+							<p>We specialize in medical sensitive areas, manufacturing clean rooms, data processing centers, and other healthcare and biotech projects. We have worked with clients across central and eastern Massachusetts since 1985. Some of our clients include Beth Israel Deaconess Medical Center, Lahey Clinic, and Lawrence General Hospital.</p>
+        <h3><a href= "about_us.html">About Us</a></h3>
+        					<p>Peluso Construction is comprised of a small team with over 20 years experience in commercial renovations in healthcare and other specialized settings. In addition to our team of experts, we have long standing relationships with professional architects and engineers for design build projects, as well as the finest mechanical, plumbing, electrical, drywall, flooring and painting subcontractors located in eastern and central Massachusetts</p>
+        <h3><a href= "contact_us.html">Contact Us</a></h3>
+        					<p>Peluso Construction works with clients across central and eastern Massachusetts. Contact us to learn more about our services.</p>
+        		
+	</div> <!-- END #mainContent -->
+	<footer>
+		Peluso Construction, Inc. is fully licensed (CS00011737) and insured.
+	</footer>
+</div> <!-- End #container -->
+</body>
+</html>
+services_and_clients.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Peluso Construction, Inc. specializes in medical sensitive areas, manufacturing clean rooms, data processing centers, and other healthcare and biotech projects." />
+	<meta name="keywords" content="Services, Experience, General Contracting, Project Management, Design and Construction, Additions, Renovations, Biotech, Healthcare, Biotechnology Manufacturing, Clean Rooms, CT and PET Scan Facilities, Data Processing and Storage Facilities, Linear Accelerators, MRI Trailers & Magnets, Radiology Suites, Massachusetts, Beth Israel Deaconess Medical Center, Harvard Vanguard Medical Associates, Lahey Clinic, Lawrence General Hospital, Aimnet Solutions Inc., Edward Kazarian, MD, The First Years Corporate Offices, Hyaluron Contract Manufacturing, Oakdale Rehab & Skilled Nursing Center, UMass Memorial Healthcare, U. S. Department of Veteran's Affairs" />
+	
+	<title>Peluso Construction, Inc.</title>
+	
+	<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+<div id="container">
+    <header>
+			<img src="images/peluso_construction_logo.gif" alt="Peluso Construction, Inc."/>
+		<nav>
+        	<ul>
+				<li><a href= "index.html">Home</a></li>
+        		<li><a href= "services_and_clients.html">Services and Clients</a></li>
+        		<li><a href= "about_us.html">About Us</a></li>
+        		<li><a href= "contact_us.html">Contact Us</a></li>
+        	</ul>
+    	</nav>
+    </header>
+    
+	<div id="mainContent">
+	
+		<!-- Main Content -->
+		
+		<h1>Construction Management Professionals</h1>
+		<h2>Services</h2>
+		
+		<p><strong>Specialized Construction needs require a specialized contractor.</strong>
+		<br/>Peluso Construction, Inc. specializes in medical sensitive areas, manufacturing clean rooms and data processing centers. We have experience in the following areas:</p>
+							<table border="1"> 
+         						<thead>
+            							<tr>
+             						 		<th>General Services</th>
+              						 		<th>Specialized Areas</th>
+           								</tr>
+          						</thead>
+          						<tbody>
+            							<tr>
+              						 		<td>General Contracting</td>
+              						 		<td>Project Management</td>
+             						 		<td>Design and Construction</td>
+             						 		<td>Additions/Renovations</td>
+             							</tr>
+            							<tr>
+              						 		<td>Biotechnology Manufacturing
+              						 		<br/>Clean Rooms</td>
+              						 		<td>CT and PET Scan Facilities</td>
+              						 		<td>Data Processing and Storage Facilities</td>
+              						 		<td>Linear Accelerators
+              						 		<br/>MRI Trailers & Magnets
+											<br/>Radiology Suites</td>
+            							</tr>
+        						</tbody>
+							</table>
+		
+		<h2>Clients</h2>
+							<p>Peluso Construction has worked with clients across Massachusetts since 1985. Below are some of our current clients:</p>
+								<ul>
+									<li><em>Beth Israel Deaconess Medical Center</em><br/>Located in Needham, MA, BIDMC is a teaching hospital of Harvard Medical School.
+										<br/><img src="images/BethIsraelDeaconessMedicalCenter.jpg" alt="Beth Israel Deaconess Medical Center" width="304" height="228"/></li>
+									<li><em>Harvard Vanguard Medical Associates</em><br/>Harvard Vanguard Medical Associates is a non-profit, multi-specialty medical group practice providing care to almost 495,000 adult and pediatric patients at more than 20 offices across eastern Massachusetts.
+										<br/><img src="images/HarvardVanguardFaulknerHospital.jpg" alt="Harvard Vanguard Faulkner Hospital" width="304" height="228"/></li>
+									<li><em>Lahey Clinic</em><br/>Lahey Clinic Medical Center in Burlington encompasses an ambulatory care center serving more than 3,000 patients each day and a 317-bed hospital. 
+										<br/><img src="images/LaheyClinic.jpg" alt="Lahey Clinic" width="304" height="228"/></li>
+									<li><em>Lawrence General Hospital</em><br/>
+										Peluso Construction helped build a $5 million Imaging Center, including the only "Ambient Experience" MRI system in the state.
+										<br/><img src="images/LawrenceGeneralHospitalMRI.jpg" alt="Lawrence General Hospital MRI" width="304" height="228"/></li>
+								</ul>
+								
+							<p> We have also worked with:</p>
+								<ul>
+									<li>Aimnet Solutions Inc.</li>
+									<li>Edward Kazarian, MD</li>
+									<li>The First Years Corporate Offices</li>
+									<li>Hyaluron Contract Manufacturing</li>
+									<li>Oakdale Rehab & Skilled Nursing Center</li>
+									<li>UMass Memorial Healthcare</li>
+									<li>U. S. Department of Veteran's Affairs</li>
+								</ul>
+							<p></p>
+							
+	</div> <!-- END #mainContent -->
+	<footer>
+		Peluso Construction, Inc. is fully licensed (CS00011737) and insured.
+	</footer>
+</div> <!-- End #container -->
+</body>
+</html>
+@Bishop-up-my-ass
