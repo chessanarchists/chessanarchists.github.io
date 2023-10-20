6c18ae1bb80682bc73c2c087c410ecaad8c6d89e
Bishop-up-my-ass <147948344+Bishop-up-my-ass@users.noreply.github.com>
2023-10-19 23:56:38 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 	<div class="flex flex-col min-h-screen">
 		<div class="flex-grow">
 			<nav class="bg-primary text-white w-screen flex items-center space-x-[1em] py-2">
-				<nuxt-link to="/"><img src="~/assets/images/horsey.png" width="50" class="ms-5 rounded" /></nuxt-link>
+				<nuxt-link to="/"><img src="~/assets/images/assets/images/Screenshot 2023-10-19 11.49.46 PM.png" width="50" class="ms-5 rounded" /></nuxt-link>
 				<div class="flex space-x-5" v-for="(page, index) in pages" :key="index">
 					<nuxt-link :to="page.url" class="p-1 bg-[#ED1824] rounded-lg
 					hover:bg-indigo-900 hover:animate-bounce">
