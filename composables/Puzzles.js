68e7de03c2a731a7e0fd46aa47e2650ec04e0d43
probablyjassin <aouanijassin@gmail.com>
2023-10-24 19:14:52 +0200

    working on puzzles.js

--- composables/Puzzles.js
+++ composables/Puzzles.js
@@ -1,6 +1,20 @@
 const { pieces } = usePieces()
 
 export function usePuzzles() {
+    const starting = {
+        position: [
+            pieces.RookB, pieces.KnightB, pieces.BishopB, pieces.QueenB, pieces.KingB, pieces.BishopB, pieces.KnightB, pieces.RookB,
+            pieces.PawnB, pieces.PawnB, pieces.PawnB, pieces.PawnB, pieces.PawnB, pieces.PawnB, pieces.PawnB, pieces.PawnB,
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            "", "", "", "", "", "", "", "",
+            pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, 
+            pieces.RookW, pieces.KnightW, pieces.BishopW, pieces.QueenW, pieces.KingW, pieces.BishopW, pieces.KnightW, pieces.RookW,
+	    ],
+        solution: ['', ''] // not implemented yet
+    }
+
     const EnPassent = {
         position: [
             pieces.RookB, pieces.KnightB, pieces.BishopB, pieces.QueenB, pieces.KingB, pieces.BishopB, pieces.KnightB, pieces.RookB,
@@ -12,7 +26,7 @@ export function usePuzzles() {
             pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, pieces.PawnW, "", pieces.PawnW, 
             pieces.RookW, "", pieces.BishopW, pieces.QueenW, pieces.KingW, pieces.BishopW, pieces.KnightW, pieces.RookW,
 	    ],
-        solution: ['', '']
+        solution: ['', ''] // not implemented yet
     }
 
     return {
