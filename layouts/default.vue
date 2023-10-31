12829eb60d2019a2183d5a01d0e679e18601db9b
probablyjassin <aouanijassin@gmail.com>
2023-10-31 17:07:06 +0100

    better navbar design

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,12 +6,12 @@ to be displayed on every page -->
 	<div class="flex flex-col min-h-screen">
 		<!-- This is the Navbar -->
 		<div class="flex-grow">
-			<nav class="bg-primary text-white w-screen flex items-center space-x-[1em] py-2">
-				<nuxt-link to="/"><img src="~/assets/images/horsey.png" width="50" class="ms-5 rounded" /></nuxt-link>
+			<nav class="bg-primary text-white w-screen flex items-center space-x-[1em] py-2 rounded-b-3xl shadow-lg">
+				<nuxt-link to="/"><img src="~/assets/images/horsey.png" width="50" class="ms-5 rounded hover:scale-110 transition-all" /></nuxt-link>
 				<div class="flex space-x-5" v-for="(page, index) in pages" :key="index">
 					<nuxt-link :to="page.url" class="p-1 bg-[#ED1824] rounded-lg
-					hover:bg-indigo-900 hover:animate-bounce">
-						{{ page.title }}
+					hover:bg-indigo-900 hover:scale-110 transition-all py-0.5 shadow-md">
+						<p class="md:mb-1">{{ page.title }}</p>
 					</nuxt-link>
 				</div>
 			</nav>
