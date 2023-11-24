e61658b4113f8baf0596b9c4a171588eb83034b9
probablyjassin <aouanijassin@gmail.com>
2023-11-24 19:16:26 +0100

    added an en passant puzzle

--- composables/Puzzles.js
+++ composables/Puzzles.js
@@ -6,10 +6,14 @@ export function usePuzzles() {
             position: convert("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"),
             solution: ["e1", "e2"],
         },
-        EnPassent: {
+        EnPassant: {
             position: convert("r1bqkbnr/ppppp1pp/2n5/4Pp2/8/8/PPPP1PPP/RNBQKBNR"),
             solution: ["e5", "f6"],
-        }
+        },
+        EnPassant2: {
+            position: convert("rnbqkbnr/1p1p1ppp/8/1Pp1p2Q/p1B1P3/8/P1PP1PPP/RNB1K1NR"),
+            solution: ["b5", "c6"]
+        },
     }
 
 	return {
