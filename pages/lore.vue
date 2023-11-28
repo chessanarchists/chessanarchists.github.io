75196abd481a2d928ded3b32ddc3eee1924865cc
probablyjassin <aouanijassin@gmail.com>
2023-11-28 17:48:13 +0100

    button selection en passant

--- pages/lore.vue
+++ pages/lore.vue
@@ -1,11 +1,32 @@
 <!-- In pages that just display text content like here, we use a 'ContentDoc' tag to grab the markdown text
 from the same-named .md file in the 'content' folder. We also import the stylesheet that applies styling to the text -->
 <template>
-	<div>
-		<input type="text" placeholder="Enter chess.c*m username" class="">
+	<div class="mt-2">
+		<div class="ms-4">
+			<div class="flex flex-wrap">
+				<span>
+					<p>Count En Passants played</p>
+					<input type="text" placeholder="Enter chess.c*m username" class="rounded-lg py-2 bg-red-200" />
+				</span>
+				<span>
+					<p>within the last ... months</p>
+					<div class="flex bg-red-500 rounded-full overflow-hidden">
+						<button 
+						class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 bg-clip-border bg-origin-border"
+						:class="{ 'bg-red-800': monthRange === option }"
+						@click="monthRange = option"
+						v-for="option in [2, 6, 12, 24]">
+						{{ option }}
+						</button>
+					</div>
+				</span>
+			</div>
+		</div>
+
 		<main>
 			<ContentDoc />
 		</main>
+		
 	</div>
 </template>
 
@@ -22,6 +43,8 @@ from the same-named .md file in the 'content' folder. We also import the stylesh
 			},
 		],
 	});
+
+	const monthRange = ref(2)
 </script>
 
 <style scoped>
