fe80f15584cc0449dc8838e315e4d582ed510529
probablyjassin <aouanijassin@gmail.com>
2023-12-01 10:00:58 +0100

    huge reddit image optimizations

--- components/Reddit.vue
+++ components/Reddit.vue
@@ -11,13 +11,13 @@ from the /composables/useReddit.js file. It provides us with the reddit posts an
 		</div>
 		<div class="flex flex-wrap justify-center space-x-[2%] p-3 bg-primary rounded-2xl shadow-2xl">
 			<div
-				v-for="(post, index) in redditPosts.filter(isImagePost).slice(0, 3)"
+				v-for="(post, index) in posts"
 				:key="index"
-				
 				class="w-screen my-2 sm:w-[30%] rounded bg-secondary flex flex-col justify-center scale-[0.9] hover:scale-[1] transition-all shadow-2xl">
-				<a :href="`https://reddit.com${post.permalink}`" draggable="false">
+			
+				<a :href="`https://reddit.com${post.url}`" draggable="false">
 					<p>{{ post.title }}</p>
-					<img :src="getImageUrl(post)" width="250" height="200" class="m-auto my-auto" draggable="false" loading="eager" fetchpriority="high"/>
+						<img :src="post.image" width="250" height="200" class="m-auto my-auto" draggable="false" loading="eager" />
 					<p>{{ post.score }} fake internet points</p>
 				</a>
 			</div>
@@ -26,19 +26,19 @@ from the /composables/useReddit.js file. It provides us with the reddit posts an
 </template>
 
 <script setup>
-	/* This effectively says "grab the redditPosts from the useReddit.js file" */
-	const { redditPosts } = useReddit();
+	const { getPosts } = useReddit()
+	const posts = ref([])
+	getPosts().then((data) => posts.value = data)
 
 	function isImagePost(post) {
-		return post.url.endsWith(".jpg") || post.url.endsWith(".png") || post.url.endsWith(".webp");
+		return post["media_metadata"];
 	}
 
 	function getImageUrl(post) {
-		if (isImagePost(post)) {
-			return post.url;
-		} else if (post.preview && post.preview.images[0].source.url) {
-			return post.preview.images[0].source.url;
-		}
-		return "";
+		console.log(post["media_metadata"])
+		let media_metadata = post["media_metadata"];
+		let images = media_metadata[Object.keys(media_metadata)[0]]["p"];
+		console.log(images)
+		return images.filter((obj) => obj["x"] < 1000).at(-1)["u"];
 	}
 </script>
--- composables/useReddit.js
+++ composables/useReddit.js
@@ -2,20 +2,30 @@ const redditPosts = ref([]);
 
 export function useReddit() {
 	async function getPosts() {
-		try {
-			const response = await fetch("https://www.reddit.com/r/anarchychess/top.json?limit=6&t=day");
-			const data = await response.json();
-			redditPosts.value = data.data.children.map((post) => post.data);
-		} catch (error) {
-			console.error("Error fetching Reddit data", error);
+		const response = await fetch("https://www.reddit.com/r/anarchychess/top.json?limit=10&t=day", {mode: 'cors'});
+		let data = await response.json();
+		data = data.data.children.map((post) => post.data);
+		let posts = [];
+		for (let [i, post] of data.entries()) {
+			if (!(post.preview) || i >= 3) continue
+
+			let img_url = post.preview.images[0].resolutions.filter((obj) => obj.width < 1000).at(-1).url
+			const img_node = document.createTextNode(img_url);
+			const image = img_node.nodeValue.replaceAll('amp;', '')
+
+			posts.push(
+				{
+					title: post.title,
+					score: post.score,
+					image: image,
+					url: post.permalink
+				}
+			)
 		}
+		return posts
 	}
 
-	onMounted(() => {
-		getPosts();
-	});
-
 	return {
-		redditPosts,
+		getPosts,
 	};
 }
