76ff087b66babad97d4a1b2d39b98e79116c241f
probablyjassin <aouanijassin@gmail.com>
2023-12-16 11:37:28 +0100

    add dark tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {
-      colors: {
-        'text': '#131217',
-        'background': '#e5e0f1',
-        'primary': '#e80001',
-        'secondary': '#f3a5a5',
-        'accent': '#200082',
-       },       
-    },
-  },
-  plugins: [],
-}
-
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				text: "#131217",
+				"background": "#e5e0f1",
+				"primary": "#e80001",
+				"secondary": "#f3a5a5",
+				"accent": "#200082",
+        
+				"text-dark": "#fcd1d1",
+				"background-dark": "#150201",
+				"primary-dark": "#ff1a1a",
+				"secondary-dark": "#5a0c0c",
+				"accent-dark": "#9f80ff",
+			},
+		},
+	},
+	plugins: [],
+};
